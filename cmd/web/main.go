995405b76fc0ddc03a50bdf9f3b8bb6981540780
João Victor Mendes <joaodev21@gmail.com>
2026-07-23 12:33:16 -0300

    fix(backend): implementa gate explicito de ambiente para mock da jikan api

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -10,21 +10,30 @@ import (
 
 	"github.com/JoaoMendes1/anideck/internal/config"
 	"github.com/JoaoMendes1/anideck/internal/database"
+	"github.com/JoaoMendes1/anideck/internal/handlers"
+	"github.com/JoaoMendes1/anideck/internal/jikan"
+
 )
 
 func main() {
-	// Carrega variáveis de ambiente e valida configuração inicial
+	//1. Carrega variáveis de ambiente e valida configuração inicial
 	if err := config.LoadAndValidateEnv(); err != nil {
 		log.Fatalf("Erro crítico no boot: %v", err)
 	}
-
-	// Inicializa a conexão com o banco de dados 
 	database.Connect()
 	log.Println("Conexão com o banco de dados estabelecida!")
 
+	// 2. Instaciamos o cliente da Jikan (Que contém o rate limiter)
+	jikanClient := jikan.NewClient()
+
+	// 3. Instaciamos nosso Handler, entregando o cliente da Jikan para ele usar
+	searchHandler := &handlers.SearchHandler{
+		JikanClient: jikanClient,
+	}
+
+	// 4. Configuração das rotas (chi)
 	// Cria o roteador principal usando o framework Chi
 	r := chi.NewRouter()
-
 	r.Use(middleware.RequestID) // gera um ID único por requisição (útil para rastrear logs)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
@@ -34,9 +43,12 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
+	// Nova rota de BUSCA
+	r.Get("/api/search", searchHandler.HandleSearch)
+
+	// Inicia o servidor
 	port := os.Getenv("PORT")
 	log.Printf("Servidor rodando na porta %s...", port)
-
 	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatalf("Erro ao iniciar o servidor: %v", err)
 	}
